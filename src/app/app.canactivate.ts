1c4dd4702e531231f86c2387ae56748ad9052aaa
sami <cse.sami07006@gmail.com>
2018-10-16 17:36:26 +0600

    Change Authentication Guard as Security

--- src/app/app.canactivate.ts
+++ src/app/app.canactivate.ts
@@ -1,14 +1,20 @@
 import { Injectable } from '@angular/core';
-import { CanActivate } from '@angular/router';
+import { CanActivate, Router } from '@angular/router';
 import { LoginService } from './services/login.service';
 
 @Injectable()
 export class CanActivateViaAuthGuard implements CanActivate {
 
-  constructor(private loginService: LoginService) {}
+  constructor(private router: Router, private loginService: LoginService) { }
 
-  
-  canActivate():any {
-    return localStorage.length>0 && localStorage.getItem("x-AuthToken")!==null && localStorage.getItem("x-AuthToken")!==undefined;
+
+  canActivate(): any {
+    let authenticated = localStorage.length > 0 && localStorage.getItem("x-AuthToken") !== null && localStorage.getItem("x-AuthToken") !== undefined;
+    if (!authenticated) {
+      this.router.navigate(['login']);
+    }
+    return authenticated;
   }
-};
+
+
+}
